4aa9a6b358d8a0618a53b9f3a84beb06f2df68d0
owner <owner@DESKTOP-EBSVCJ9>
2017-06-24 16:47:18 +0900

    aa

--- data_helpers.py
+++ data_helpers.py
@@ -19,7 +19,7 @@ def clean_str(string):
     string = re.sub(r",", " , ", string)
     string = re.sub(r"!", " ! ", string)
     string = re.sub(r"\(", " \( ", string)
-    string = re.sub(r"\)", " \) ", string)
+    string = re.sub(r"\)", " \) ", string) 
     string = re.sub(r"\?", " \? ", string)
     string = re.sub(r"\s{2,}", " ", string)
     return string.strip().lower()
